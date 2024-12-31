f8b6cb9e3dc50a9ee955841bda6bc7b4979d4c86
webgaurav02 <gauravcodes123@gmail.com>
2024-12-31 19:03:19 +0530

    Fix same day event not showing

--- src/app/api/events/fetchevents/route.js
+++ src/app/api/events/fetchevents/route.js
@@ -1,7 +1,7 @@
 import connectMongo from '../../../../lib/mongodb';
 import Event from '../../../../models/Event';
 
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 
 export const GET = async (req) => {
   try {
@@ -17,10 +17,12 @@ export const GET = async (req) => {
     if (city) filter.city = city;
 
     // Fetch events based on the filter and sort according to date
-    const events = await Event.find(filter)
+    const events = await Event.find(filter);
 
-    // Get the current date
+    // Get the current date and add one day
     const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() - 1);
+
 
     // Separate events into upcoming and past events
     const upcomingEvents = events.filter(event => new Date(event.date) >= currentDate);
@@ -32,10 +34,8 @@ export const GET = async (req) => {
     // Sort past events in descending order
     pastEvents.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-
     // Return the events in the response
     return new Response(JSON.stringify({ success: true, upcomingEvents: upcomingEvents, pastEvents: pastEvents }), { status: 200 });
-  
   
   } catch (error) {
     // Handle errors
