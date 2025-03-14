d46cf695e04d3ffc581569954ed438ebc4c0cced
Chimmon Ghosh Pakma <chimmon.003755@snuindia.in>
2025-03-14 15:00:36 +0530

    Update HeroSection.js

--- src/app/components/HeroSection.js
+++ src/app/components/HeroSection.js
@@ -3,25 +3,39 @@ import React from 'react';
 //Components
 import VideoComponent from './VideoComponent';
 
+import banner from "../../../public/banner.jpeg"
+import Image from 'next/image';
+
 const HeroSection = () => {
   return (
-    <div className='flex lg:flex-row flex-col items-center min-h-[70svh] md:px-60'>
-      {/* <VideoComponent /> */}
-      <div className='flex flex-col justify-center min-h-[70svh] items-center mx-auto px-5 md:px-0'>
+    <div className='flex lg:flex-row flex-col items-center min-h-[70svh]'>
+      <div className='mx-auto'>
+        <Image
+          src={banner}
+          alt="Onlybees Sports Portal"
+          width="0"
+          height="0"
+          sizes="100svw"
+          className="md:w-[70svw] w-[90svw] h-auto object-contain mx-auto" 
+        />
+        <div className='md:text-4xl text-2xl font-coolvetica bg-black text-white px-5 py-2 w-fit mx-auto my-10 rounded-lg'>COMING SOON</div>
+      </div>
+      {/* <VideoComponent />
+      <div className='flex flex-col md:w-[37rem] mx-auto px-10 md:px-0'>
         <div className="h-fit overflow-y-hidden">
-          <h1 className='font-coolvetica md:text-8xl text-[4.3rem] mt-5 md:mt-0 md:text-center leading-none animate-riseUp'>
+          <h1 className='font-coolvetica md:text-6xl text-[3rem] mt-10 md:mt-0 leading-none animate-riseUp'>
             WE ARE THE DOERS,
           </h1>
         </div>
         <div className="h-fit overflow-y-hidden">
-          <h1 className='font-coolvetica md:text-8xl text-[4.3rem] leading-none md:text-center animate-riseUp'>
+          <h1 className='font-coolvetica md:text-6xl text-[3rem] leading-none animate-riseUp'>
             THINKERS, DREAMERS
           </h1>
         </div>
-        <p className='mt-2 md:text-[1.3rem] text-[1.1rem] md:leading-snug leading-tight text-justify font-coolvetica text-[#8B8A8A] opacity-85'>
+        <p className='md:w-[37rem] mt-2 md:text-[1.3rem] text-[1.1rem] md:leading-snug leading-tight text-justify font-coolvetica text-[#8B8A8A] opacity-85'>
           THIS IS HOW WE PICTURE IT. A SWARM OF BEES BUILDING A HIVE TO SHARE AN ECOSYSTEM. THERE IS SOMETHING INTRIGUING ABOUT THE &quot;DANCE LANGUAGE&quot; OF BEES. AND OUR TEAM IS DEDICATED TO COMMUNICATE AND BUILD A CREATIVE ECOSYSTEM IN THE MOST EFFECTIVE AND IMPACTFUL WAY.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
